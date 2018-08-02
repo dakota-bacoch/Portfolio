ff15fa08f703b92257bde830ce952f5cdbd7933d
dakota-bacoch <bacoch1998@gmail.com>
2018-08-02 07:12:06 -0700

    frontpage

---
+++ js/main.js
@@ -0,0 +1,77 @@
+window.onload = function() { 
+
+//tipped
+
+
+
+
+
+//Jquery load delay
+
+    $("#name").hide(1).delay(200).show(1);
+    $("#buttons").hide(1).delay(200).show(1);
+
+//Anime 
+
+anime({
+    targets: "#D",
+    translateY: [
+        { value: -1200, duration: 500},
+        { value: 0, duration: 1000},
+    ],
+  });
+anime({
+    targets: "#A",
+    translateY: [
+        { value: -1200, duration: 600},
+        { value: 0, duration: 1000},
+    ],
+  });
+anime({
+    targets: "#K",
+    translateY: [
+        { value: -1200, duration: 700},
+        { value: 0, duration: 1000},
+    ],
+  });
+anime({
+    targets: "#O",
+    translateY: [
+        { value: -1200, duration: 800},
+        { value: 0, duration: 1000},
+    ],
+  });
+anime({
+    targets: "#T",
+    translateY: [
+        { value: -1200, duration: 900},
+        { value: 0, duration: 1000},
+    ],
+  });
+anime({
+    targets: "#A2",
+    translateY: [
+        { value: -1200, duration: 1000},
+        { value: 0, duration: 1000},
+    ],
+  });
+  anime({
+    targets: "#buttons",
+    translateY: [
+        { value: 350, duration: 1300},
+        { value: 0, duration: 2300},
+    ],
+  });
+}
+
+$(document).ready(function() {
+    setTimeout(function() {
+        Tipped.create('#button1', 'About me', { position: 'bottom', size: 'large'});
+        Tipped.create('#button2', 'Portfolio', { position: 'bottom', size: 'large' });
+        Tipped.create('#button3', 'Contact', { position: 'bottom', size: 'large' });
+        Tipped.create('#button4', 'Github', { position: 'bottom', size: 'large' });
+
+   }, 2000);
+    
+    
+    });
